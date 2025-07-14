39be4db8838f8a302195fc01d77d46bf05cb5884
Bao Nguyen Nguyen <baonguyen@MacBookPro.lan>
2025-07-13 22:23:10 -0500

    first commit - basic

---
+++ src/test/java/basic/ASimpleTest.java
@@ -0,0 +1,53 @@
+package basic;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ASimpleTest {
+
+    Playwright playwright;
+    Browser browser;
+    Page page;
+
+    @BeforeEach
+    void setup() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+        page = browser.newPage();
+    }
+
+    @AfterEach
+    void teardown() {
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    void shouldShowThePageTitle(){
+        page.navigate("https://practicesoftwaretesting.com/");
+        String title = page.title();
+
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+    }
+
+    @Test
+    void shouldSearchByKeyword(){
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com/");
+        page.locator("[placeholder = Search]").fill("pliers");
+        page.locator("button:has-text('Search')").click();
+
+        int matchingSearchResult = page.locator(".card").count();
+
+        Assertions.assertTrue(matchingSearchResult > 0);
+    }
+
+}
