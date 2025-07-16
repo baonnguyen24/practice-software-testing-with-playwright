639f2523401eb9a902eaeda5f8ef7bfd9566f82c
Bao Nguyen Nguyen <baonguyen@MacBookPro.lan>
2025-07-15 22:12:13 -0500

    add more cases to demonstrate wait

--- src/test/java/basic/PlaywrightWaits.java
+++ src/test/java/basic/PlaywrightWaits.java
@@ -1,6 +1,9 @@
 package basic;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.WaitForSelectorState;
 import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
@@ -66,5 +69,45 @@ public class PlaywrightWaits {
         Assertions.assertThat(productTitles).contains("Pliers", "Bolt Cutters", "Hammer");
     }
 
+    @Test
+    @DisplayName("Should filter product by category")
+    void shouldFilterProductByCategory() {
+        openPage();
+        page.getByRole(AriaRole.MENUBAR).getByText("Categories").click();
+        page.getByRole(AriaRole.MENUBAR).getByText("Power Tools").click();
+
+        // LESSON LEARN: The page will take a while to load all components. So need to set a wait here
+        page.waitForSelector(".card",
+                new Page.WaitForSelectorOptions().setState(WaitForSelectorState.VISIBLE));
+
+        var filteredProduct = page.locator(".card-title").allInnerTexts();
+
+        Assertions.assertThat(filteredProduct).contains("Sheet Sander", "Belt Sander");
+    }
+
+    @Test
+    @DisplayName("Waiting for element to appear and disappear")
+    void shouldDisplayToasterMessage() {
+        openPage();
+        page.getByText("Bolt Cutters").click();
+        page.getByText("Add to cart").click();
+
+        // Wait for Toaster message to appear
+        PlaywrightAssertions.assertThat(page.getByRole(AriaRole.ALERT)).isVisible();
+        PlaywrightAssertions.assertThat(page.getByRole(AriaRole.ALERT)).hasText("Product added to shopping cart.");
+
+        // Wait for Toaster message to disappear
+        page.waitForCondition( () -> page.getByRole(AriaRole.ALERT).isHidden());
+    }
+
+    @Test
+    @DisplayName("Should update the cart item count")
+    void shouldUpdateCartItemCount() {
+        openPage();
+        page.getByText("Bolt Cutters").click();
+        page.getByText("Add to cart").click();
+
+        page.waitForSelector("[data-test='cart-quantity']:has-text('1')");
+    }
 
 }
